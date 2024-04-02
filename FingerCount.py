4be766fe58f3efd937c7685477191ea6a60069d2
Sarthak Waliwadekar <128313088+Sarthak2578@users.noreply.github.com>
2024-04-02 12:54:07 +0530

    Update FingerCount.py

--- FingerCount.py
+++ FingerCount.py
@@ -7,7 +7,7 @@ background = None
 #Rate of background update
 accumulated_weight = 0.5
 
-#Manually setting the region of interest
+#Region of Interest
 roi_top = 20
 roi_bottom = 300
 roi_right = 300
@@ -17,56 +17,43 @@ def calc_accum_avg(frame, accumulated_weight):
 
     global background
 
-    # Creating background from a copy of the frame
     if background is None:
         background = frame.copy().astype("float")
         return None
-
-    # compute weighted average, accumulate it and update the background
     cv2.accumulateWeighted(frame, background, accumulated_weight)
 
 def segment(frame, threshold=25):
     global background
 
-    # Calculates the Absolute Difference between the background and the current frame
     diff = cv2.absdiff(background.astype("uint8"), frame)
 
-    # Applying a threshold to the image so we can grab the foreground
     _, thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
 
-    # Grabbing the external contours form the image (Looking for hand in black and white)
     contours, hierarchy = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # If length of contours list is 0, then no hand
     if len(contours) == 0:
         return None
     else:
-        # Chooses the biggest hand-like shape in the frame
         hand_segment = max(contours, key=cv2.contourArea)
-
-        # Return both the hand segment and the thresholded hand image
+        
         return (thresholded, hand_segment)
 
 def count_fingers(thresholded, hand_segment):
 
 
-    # Calculated the convex hull of the hand segment
     conv_hull = cv2.convexHull(hand_segment)
 
-    # Finding the extreme points and arranging in a tuple
     top    = tuple(conv_hull[conv_hull[:, :, 1].argmin()][0])
     bottom = tuple(conv_hull[conv_hull[:, :, 1].argmax()][0])
     left   = tuple(conv_hull[conv_hull[:, :, 0].argmin()][0])
     right  = tuple(conv_hull[conv_hull[:, :, 0].argmax()][0])
 
-    # Calculating center of the hand (Half)
     cX = (left[0] + right[0]) // 2
     cY = (top[1] + bottom[1]) // 2
 
     # find the maximum euclidean distance between the center of the palm
     # and the most extreme points of the convex hull
 
-    # Calculate the Euclidean Distance between the center of the hand and the left, right, top, and bottom.
     distance = pairwise.euclidean_distances([(cX, cY)], Y=[left, right, top, bottom])[0]
 
     # largest distance
@@ -79,13 +66,10 @@ def count_fingers(thresholded, hand_segment):
     # grabbing an ROI of only that circle
     circular_roi = np.zeros(thresholded.shape[:2], dtype="uint8")
 
-    # draw the circular ROI
     cv2.circle(circular_roi, (cX, cY), radius, 255, 10)
 
-    # Isolates hand inside the circle
     circular_roi = cv2.bitwise_and(thresholded, thresholded, mask=circular_roi)
 
-    # Grab contours in circle ROI (Finger outline detection)
     contours, hierarchy = cv2.findContours(circular_roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     # Finger count starts at 0
@@ -94,10 +78,7 @@ def count_fingers(thresholded, hand_segment):
     # loop through the contours to see if we count any more fingers.
     for cnt in contours:
 
-        # Bounding box of countour
         (x, y, w, h) = cv2.boundingRect(cnt)
-
-        # Increment count of fingers based on two conditions:
 
         # 1. Contour region is not the very bottom of hand area (the wrist)
         out_of_wrist = ((cY + (cY * 0.25)) > (y + h))
@@ -113,32 +94,22 @@ def count_fingers(thresholded, hand_segment):
 
 cam = cv2.VideoCapture(0)
 
-# Intialize a frame count
 num_frames = 0
 
-# keep looping, until interrupted
 while True:
-    # get the current frame
     ret, frame = cam.read()
 
     if not ret:
-        continue  # Skip this iteration and try capturing the next frame
+        continue  
 
-    # flip the frame so that it is not the mirror view
     frame = cv2.flip(frame, 1)
 
+    #frame = cv2.flip(frame, 1)
 
-
-    # flip the frame so that it is not the mirror view
-    frame = cv2.flip(frame, 1)
-
-    # clone the frame
     frame_copy = frame.copy()
 
-    # Grab the ROI from the frame
     roi = frame[roi_top:roi_bottom, roi_right:roi_left]
 
-    # Apply grayscale and blur to ROI
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
@@ -156,31 +127,22 @@ while True:
         # segment the hand region
         hand = segment(gray)
 
-        # First check if we were able to actually detect a hand
         if hand is not None:
 
-            # unpack
             thresholded, hand_segment = hand
 
-            # Draw contours around hand segment
             cv2.drawContours(frame_copy, [hand_segment + (roi_right, roi_top)], -1, (255, 0, 0),1)
 
-            # Count the fingers
             fingers = count_fingers(thresholded, hand_segment)
 
-            # Display count
             cv2.putText(frame_copy, str(fingers), (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
 
-            # Also display the thresholded image
             cv2.imshow("Thesholded", thresholded)
 
-    # Draw ROI Rectangle on frame copy
     cv2.rectangle(frame_copy, (roi_left, roi_top), (roi_right, roi_bottom), (0,0,255), 5)
 
-    # increment the number of frames for tracking
     num_frames += 1
 
-    # Display the frame with segmented hand
     cv2.imshow("Finger Count", frame_copy)
 
 
